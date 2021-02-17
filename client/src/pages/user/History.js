224dffc6dd87dcac5b0d40c3081b0495c1963f29
MatthewSines <thunor13@ufl.edu>
2021-02-17 08:17:34 -0800

    added user navigation bar and password reset and mocked up Wishlist page

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,11 +1,15 @@
 //this is the user dashboard, and it will be part of a protected route like
 //done with Login and Register when user is logged in
 import React from "react";
+import UserNav from "../../components/nav/UserNav";
 
 const History = () => {
   return (
     <div className="container-fluid">
       <div className="row">
+        <div className="col-md-2">
+          <UserNav />
+        </div>
         <div className="col">user History page</div>
       </div>
     </div>
